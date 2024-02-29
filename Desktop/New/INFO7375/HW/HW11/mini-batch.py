9718b5320d42b7f08844ee037a4baab12c5bfa8e
RanAn <anran170@126.com>
2024-02-29 00:37:44 -0800

    hw11

--- Desktop/New/INFO7375/HW/HW11/mini-batch.py
+++ Desktop/New/INFO7375/HW/HW11/mini-batch.py
@@ -3,28 +3,31 @@
 import numpy as np
 
 
-def create_mini_batches(X, y, batch_size):
-    """Create mini-batches from the dataset (X, y)
-
-    Args:
+"""
+Args:
     X (np.array): Input features of shape (num_examples, num_features)
     y (np.array): Output labels of shape (num_examples, 1)
     batch_size (int): The size of each mini-batch
 
-    Returns:
+Returns:
     list: A list of tuples (mini_batch_X, mini_batch_y)
-    """
+"""
+
+
+def create_mini_batches(X, y, batch_size):
     mini_batches = []
     data = np.hstack((X, y))
     np.random.shuffle(data)
+    # number of complete mini-batches
     n_minibatches = data.shape[0] // batch_size
 
     for i in range(n_minibatches):
         mini_batch = data[i * batch_size : (i + 1) * batch_size, :]
         X_mini = mini_batch[:, :-1]
-        Y_mini = mini_batch[:, -1].reshape((-1, 1))
+        Y_mini = mini_batch[:, -1].reshape((-1, 1))  # reshape into column vector
         mini_batches.append((X_mini, Y_mini))
 
+    # Remaining examples that cannot fit into a complete mini-batch
     if data.shape[0] % batch_size != 0:
         mini_batch = data[n_minibatches * batch_size : data.shape[0]]
         X_mini = mini_batch[:, :-1]
